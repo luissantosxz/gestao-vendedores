8b9f63d934c2cf20963acfdb83e5d6f754c7c567
luissantosx <luisf3rnando29@outlook.com>
2026-02-17 19:19:15 -0300

    Adicionado migration tabela pedido

---
+++ src/main/resources/db/migration/V3_create_table_pedido.sql
@@ -0,0 +1,11 @@
+CREATE TABLE pedido(
+    id UUID PRIMARY KEY,
+    vendedor_id UUID NOT NULL,
+    valor DOUBLE PRECISION NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    data_pedido TIMESTAMP NOT NULL,
+
+    CONSTRAINT fk_pedido_vendedor
+        FOREIGN KEY (vendedor_id)
+        REFERENCES vendedor_id
+);
